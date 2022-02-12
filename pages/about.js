31c7fbeb945ffc1616bb1c648cef19605fae3908
The Noisy Duck <thenoisyduck@Thes-MacBook-Pro.local>
2022-02-12 12:40:37 +0800

    creating new route for about page

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque officiis magnam expedita iure quaerat, explicabo perspiciatis tenetur amet. Totam dolorum ducimus blanditiis quod corrupti similique temporibus quisquam, quasi eligendi fuga!</p>
+        </div>
+    );
+}
+ 
+export default About;
