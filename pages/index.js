ac0dd750c5b6b4a2824d729f1fbd062682a2bf7f
The Noisy Duck <thenoisyduck@Thes-MacBook-Pro.local>
2022-02-12 14:07:23 +0800

    creating layout component for header and footer for all pages

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae earum, minus rerum sunt soluta iusto asperiores illum aliquam quos, assumenda at, quae repellat modi molestiae in odit porro illo reprehenderit.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eum maiores molestiae, assumenda omnis quidem, distinctio dolores repellat ipsam at quam necessitatibus optio veniam eveniet sequi incidunt ea quo. Tempora.</p>
       <Link href="/ninjas"><a>See Ninja Listing</a></Link>
-      <Footer />
     </div>
   )
 }
