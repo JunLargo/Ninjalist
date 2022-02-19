8dd060bea7cd449fd9b7892f77da097841e16b98
The Noisy Duck <thenoisyduck@Thes-MacBook-Pro.local>
2022-02-19 13:42:43 +0800

    fetching data from jsonplaceholder and adding styles to all ninja list page

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
     <>
       <Head>
         <title>Ninja List | Home</title>
+        <meta name="keywords" content="Ninjas" />
       </Head>
       <div>
         <h1 className={styles.title}>Homepage</h1>
