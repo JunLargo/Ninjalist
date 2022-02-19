534b5165e3d2ed8532891710d355df473067dcaf
The Noisy Duck <thenoisyduck@Thes-MacBook-Pro.local>
2022-02-19 20:53:23 +0800

    updating footer with jlargo name

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            Copyright 2022 Ninja List
+            Copyright 2022 Ninja List | Created By: jlargo
         </footer>
     );
 }
