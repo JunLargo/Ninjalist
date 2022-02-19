3003a3c4d2ade3712de6282dbb406b5d3dd56dbc
The Noisy Duck <thenoisyduck@Thes-MacBook-Pro.local>
2022-02-19 20:03:58 +0800

    creating a static paths based on ninja id

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,4 +1,19 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
 
+    const path = data.map(ninja => {
+        return {
+            params: {id: ninja.id.toString()}
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+
+}
 
 const Details = () => {
     return (
